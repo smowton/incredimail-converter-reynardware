7afc5d6e521271b702218db339e5d19b3c2c0da1
Reynardware <Reynardware@b5a3c416-1fe5-11de-a7a1-25a91d9625e5>
2009-04-12 19:18:16 +0000

    Version 0.2

---
+++ tags/0.2/Source/Incredimail_Convert.c
@@ -0,0 +1,287 @@
+//***********************************************************************************************
+//     The contents of this file are subject to the Mozilla Public License
+//     Version 1.1 (the "License"); you may not use this file except in
+//     compliance with the License. You may obtain a copy of the License at
+//     http://www.mozilla.org/MPL/
+//
+//     Software distributed under the License is distributed on an "AS IS"
+//     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//     License for the specific language governing rights and limitations
+//     under the License.
+//
+//     The Original Code is ReynardWare Incredimail Converter.
+//
+//     The Initial Developer of the Original Code is David P. Owczarski, created March 20, 2009
+//
+//     Contributor(s):
+//
+//************************************************************************************************
+#define _CRT_SECURE_NO_WARNINGS  // remove the new mircosoft security warnings
+
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "Increadimail_Convert.h"
+
+const char *ATTACHMENT = "----------[%ImFilePath%]----------";
+
+int email_count( char *filename ) {
+int dummy = 1;
+int i;
+int e_count = 0;
+HANDLE helping_hand;
+char version[5];
+unsigned int size;
+unsigned int file_size;
+
+   ZeroMemory( &version, sizeof( version ) );
+   helping_hand = CreateFile( filename, (DWORD) GENERIC_READ, (DWORD) NULL, (DWORD) NULL, (DWORD) OPEN_ALWAYS, (DWORD) FILE_ATTRIBUTE_NORMAL, (DWORD) NULL );
+   
+   if( helping_hand ) {
+      ReadFile( helping_hand, &version, 0x04, &dummy, NULL );
+   }
+
+   SetFilePointer( helping_hand, 0, NULL, FILE_BEGIN );
+   do {
+      SetFilePointer( helping_hand, 0x26, NULL, FILE_CURRENT );  // header
+
+      for( i = 0; i < 3; i++ ) {
+         ReadFile( helping_hand, &size, 0x04, &dummy, NULL );
+         if( !strncmp( version, "V#05", 4 ) ) {
+            SetFilePointer( helping_hand, size * 2, NULL, FILE_CURRENT );
+         } else {
+            SetFilePointer( helping_hand, size, NULL, FILE_CURRENT );
+         }
+      }
+
+      SetFilePointer( helping_hand, 0x0C, NULL, FILE_CURRENT );      
+      ReadFile( helping_hand, &size, 0x04, &dummy, NULL );
+      if( !strncmp( version, "V#05", 4 ) ) {
+         SetFilePointer( helping_hand, size * 2, NULL, FILE_CURRENT );
+      } else {
+         SetFilePointer( helping_hand, size, NULL, FILE_CURRENT );
+      }
+
+      SetFilePointer( helping_hand, 0x06, NULL, FILE_CURRENT );
+      ReadFile( helping_hand, &file_size, 0x04, &dummy, NULL );
+      SetFilePointer( helping_hand, 0x08, NULL, FILE_CURRENT );
+      e_count++;
+   } while( dummy );
+   CloseHandle( helping_hand );
+   e_count--;
+
+
+return e_count;
+}
+
+
+void get_email_offset_and_size( char *filename, unsigned int *file_offset, unsigned int *size, int email_index, int e_count ) {
+HANDLE helping_hand;
+int dummy = 1;
+int *offset;
+int i, j;
+char version[5];
+unsigned int file_size, sizer;
+
+   offset = calloc( e_count, sizeof( int ) );
+   ZeroMemory( &version, sizeof( char ) * 5 );
+
+   helping_hand = CreateFile( filename, GENERIC_READ, 0x00000000, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL );
+   ReadFile( helping_hand, &version, 0x04, &dummy, NULL );
+   
+   SetFilePointer( helping_hand, 0, NULL, FILE_BEGIN );
+   for( i = 0; i < e_count; i++ ) {
+      sizer = 0;
+      SetFilePointer( helping_hand, 0x26, NULL, FILE_CURRENT );  // header
+
+      for( j = 0; j < 3; j++ ) {
+         ReadFile( helping_hand, &sizer, 0x04, &dummy, NULL );
+         if( !strncmp( version, "V#05", 4 ) ) {
+            SetFilePointer( helping_hand, sizer * 2, NULL, FILE_CURRENT );
+         } else {
+            SetFilePointer( helping_hand, sizer, NULL, FILE_CURRENT );
+         }
+      }
+
+      SetFilePointer( helping_hand, 0x0C, NULL, FILE_CURRENT );      
+      ReadFile( helping_hand, &sizer, 0x04, &dummy, NULL );
+      if( !strncmp( version, "V#05", 4 ) ) {
+         SetFilePointer( helping_hand, sizer * 2, NULL, FILE_CURRENT );
+      } else {
+         SetFilePointer( helping_hand, sizer, NULL, FILE_CURRENT );
+      }
+      SetFilePointer( helping_hand, 0x06, NULL, FILE_CURRENT );
+      file_size = 0;
+      ReadFile( helping_hand, &file_size, 0x04, &dummy, NULL );
+      offset[i] = file_size;
+      SetFilePointer( helping_hand, 0x08, NULL, FILE_CURRENT );
+   }
+   CloseHandle( helping_hand );
+
+   *size = offset[email_index];
+   for( i = 0; i < email_index; i++ ) {
+      *file_offset += offset[i];
+   }
+
+   free( offset );
+}
+
+
+void extract_eml_files( char *filename_data, char *eml_filename, int offset, unsigned int size ) {
+HANDLE helping_hand;
+HANDLE writing_hand;
+int j, k, dummy;
+char extract_data[1024];
+
+      helping_hand = CreateFile( filename_data, GENERIC_READ, NULL, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL );
+      writing_hand = CreateFile( eml_filename, GENERIC_WRITE, NULL, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL );
+      SetFilePointer( helping_hand, offset, NULL, FILE_BEGIN );
+      k = size / 1024;
+      for( j = 0; j < k; j++ ) {
+         ReadFile( helping_hand, &extract_data, 1024, &dummy, NULL );
+         WriteFile( writing_hand, &extract_data, 1024, &dummy, NULL );
+      }
+      k = size % 1024;
+      ReadFile( helping_hand, &extract_data, k, &dummy, NULL );
+      WriteFile( writing_hand, &extract_data, k, &dummy, NULL );
+      CloseHandle( helping_hand );
+      CloseHandle( writing_hand );
+}
+
+
+
+void encodeblock( unsigned char in[3], unsigned char out[4], int len )
+{
+    out[0] = cb64[ in[0] >> 2 ];
+    out[1] = cb64[ ((in[0] & 0x03) << 4) | ((in[1] & 0xf0) >> 4) ];
+    out[2] = (unsigned char) (len > 1 ? cb64[ ((in[1] & 0x0f) << 2) | ((in[2] & 0xc0) >> 6) ] : '=');
+    out[3] = (unsigned char) (len > 2 ? cb64[ in[2] & 0x3f ] : '=');
+}
+
+
+void encode( FILE *infile, FILE *outfile, int linesize )
+{
+    unsigned char in[3], out[4];
+    int i, len, blocksout = 0;
+
+    while( !feof( infile ) ) {
+        len = 0;
+        for( i = 0; i < 3; i++ ) {
+            in[i] = (unsigned char) getc( infile );
+            if( !feof( infile ) ) {
+                len++;
+            }
+            else {
+                in[i] = 0;
+            }
+        }
+        if( len ) {
+            encodeblock( in, out, len );
+            for( i = 0; i < 4; i++ ) {
+                putc( out[i], outfile );
+            }
+            blocksout++;
+        }
+        if( blocksout >= (linesize/4) || feof( infile ) ) {
+            if( blocksout ) {
+                fprintf( outfile, "\n" );
+            }
+            blocksout = 0;
+        }
+    }
+}
+
+
+void insert_attachments( char *eml_filename, char *attachments_path, char *final_email_filename ) {
+FILE *input_file;
+FILE *output_file;
+
+FILE *encode64_input_file;
+FILE *encode64_output_file;
+
+char string_1[512];
+char string_2[512];
+char attachment_name[512];
+int attachment_length;
+
+   fopen_s(&input_file, eml_filename, "r" );
+
+   if( input_file ) {
+      while( !feof( input_file ) ) {
+         memset( string_1, 0, MAX_CHAR );                             
+         fgets( string_1, MAX_CHAR - 1, input_file );
+         if( strncmp( ATTACHMENT,  string_1, 34 ) == 0 ) {
+            attachment_length = (int) strlen(string_1);
+            strcpy( attachment_name, attachments_path );
+            strcat( attachment_name, "\\" );
+            strncat( attachment_name, &string_1[34], attachment_length - 35 ); 
+            fopen_s(&encode64_input_file, attachment_name, "rb" );
+            if( encode64_input_file ) {
+               fopen_s(&encode64_output_file, "attachment.bin", "a+" );
+               encode( encode64_input_file, encode64_output_file, 70 );
+               fclose( encode64_input_file );
+               fclose( encode64_output_file );
+            
+               fopen_s(&encode64_output_file, "attachment.bin", "r" );
+               if( encode64_output_file ) {
+                  while( !feof( encode64_output_file ) ) {
+                     memset( string_2, 0, MAX_CHAR );                             
+                     fgets( string_2, MAX_CHAR - 1, encode64_output_file );
+                     fopen_s(&output_file, final_email_filename, "a+" );
+                     if( output_file ) {
+                        fprintf( output_file, "%s", string_2 );
+                        fclose( output_file );
+                     }
+                  }
+               }
+               fclose( encode64_output_file );
+               DeleteFile( "attachment.bin" );
+            }
+         } else {
+            fopen_s(&output_file, final_email_filename, "a+" );
+            if( output_file ) {
+               fprintf( output_file, "%s", string_1 );
+               fclose( output_file );
+            }
+         }
+      }
+   }
+   fclose( input_file );
+}
+
+void get_database_version(char *database, char *version ) {
+HANDLE helping_hand;
+int dummy = 1;
+char temp_version[5];
+
+   ZeroMemory( &temp_version, sizeof( temp_version ) );
+   helping_hand = CreateFile( database, GENERIC_READ, 0x00000000, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL );
+   ReadFile( helping_hand, &temp_version, 0x04, &dummy, NULL );
+   CloseHandle( helping_hand );
+
+   strcpy( version, temp_version );
+
+}
+
+int DeleteDirectory(const char *sPath) {
+  SHFILEOPSTRUCT fileop;
+  int ret;
+
+  ZeroMemory( &fileop, sizeof( SHFILEOPSTRUCT ) );
+
+  fileop.hwnd                  = NULL;
+  fileop.wFunc                 = FO_DELETE;
+  fileop.pTo                   = NULL;
+  fileop.fFlags                = FOF_NOCONFIRMATION|FOF_SILENT;  
+  fileop.fAnyOperationsAborted = FALSE;
+  fileop.lpszProgressTitle     = NULL;
+  fileop.hNameMappings         = NULL;
+
+  fileop.pFrom = sPath;
+
+  ret = SHFileOperation(&fileop);
+
+  return( ret );
+}
+
