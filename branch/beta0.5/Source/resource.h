f9022efa85a18b955dd9538bbe0a79bb7068f459
Reynardware <Reynardware@b5a3c416-1fe5-11de-a7a1-25a91d9625e5>
2010-05-07 13:14:33 +0000

    Experimenting with SQLite and major reworking of the internals

---
+++ branch/beta0.5/Source/resource.h
@@ -0,0 +1,53 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by resource.rc
+//
+#define IDD_DIALOG1                     102
+#define IDI_REY                         115
+#define IDI_REYNARD                     115
+#define IDB_BITMAP1                     119
+#define IDB_BITMAP2                     120
+#define IDB_BITMAP3                     121
+#define IDB_BITMAP4                     125
+#define IDB_BITMAP5                     126
+#define IDB_BITMAP6                     128
+#define IDB_BITMAP7                     130
+#define IDB_BITMAP8                     131
+#define IDC_BROWSE                      1005
+#define IDC_EDIT1                       1006
+#define IDC_BROWSE2                     1007
+#define IDC_EDIT2                       1008
+#define IDC_PROGRESS1                   1009
+#define IDC_ECOUNT                      1010
+#define IDC_XOFX                        1011
+#define IDC_ABOUT                       1016
+#define IDD_ABOUT                       1016
+#define IDC_RICHEDIT26                  1019
+#define IDC_STATIC6                     1020
+#define IDC_CHECK1                      1021
+#define IDC_STATIC8                     1022
+#define IDC_ANIMATE1                    1023
+#define IDC_STATIC9                     1024
+#define IDC_EMAIL_LINK                  1026
+#define IDC_WWW_LINK                    1027
+#define IDC_TITLE                       1028
+#define IDC_VERSION                     1029
+#define IDC_RADIO1                      1030
+#define IDC_EXPORT_FILE                 1030
+#define IDC_RADIO2                      1031
+#define IDC_EXPORT_DIRECTORY            1031
+#define IDC_DATABASE_NAME               1032
+#define IDC_PROGRESS2                   1033
+#define IDC_OVERALL_PROGRESS            1034
+#define IDC_OVERALL_PERCENT             1035
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1036
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
